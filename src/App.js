b12058563ba8275d0801d25aed8a8967c8ae1eb8
MD-ANIKS <freelanceranik1879@gmail.com>
2023-06-20 14:57:52 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Navbar from './components/Navbar';
 import Common from './components/Common';
 import growImg from './images/grow.svg';
-import aboutImg from '../src/images/about.svg';
+import aboutImg from './images/about.svg';
 import Services from "./components/Services";
 
 import { Routes, Route, Navigate } from 'react-router-dom';
@@ -22,7 +22,7 @@ function App() {
 
 
       <Routes>
-        <Route exact path='/' element={<Common title='Thrive and Expand your Business with' visit='/services' btn='Get Started' img={growImg} />} />
+        <Route exact path='/' element={<Common title='Grow your Business with' visit='/services' btn='Get Started' img={growImg} />} />
         <Route exact path='/about' element={<Common title='Welcome to about page' visit='/contact' btn='Contact Here' img={aboutImg} />} />
         <Route exact path='/services' element={<Services/>} />
         <Route exact path="/contact" element={<Contact/>} />
